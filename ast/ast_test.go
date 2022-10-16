dfc369a2d2ed18a01a7c329943067beee040786a
Mostain Billah <bill.rassel@gmail.com>
2022-10-16 18:24:42 +0600

    TestInputValueDefinition2 added

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -36,3 +36,37 @@ func TestInputValueDefinition(t *testing.T) {
 	}
 
 }
+
+func TestInputValueDefinition2(t *testing.T) {
+
+	test := []struct {
+		ivd            InputValueDefinition
+		expectedOutput string
+	}{
+		{
+			InputValueDefinition{Name: &Name{Kind: "Name", Token: token.Token{}, Value: "name"}, Type: &NamedType{Kind: NAMED_TYPE, Token: token.Token{}, Name: &Name{Kind: "Name", Token: token.Token{}, Value: "String"}}},
+			"name: String",
+		},
+		{
+			InputValueDefinition{Name: &Name{Kind: "Name", Token: token.Token{}, Value: "name"}, Type: &NonNullType{Kind: NONNULL_TYPE, Token: token.Token{}, Type: &NamedType{Kind: "Name", Token: token.Token{}, Name: &Name{Kind: "Name", Token: token.Token{}, Value: "String"}}}},
+			"name: String!",
+		},
+
+		{
+			InputValueDefinition{Name: &Name{Kind: "Name", Token: token.Token{}, Value: "name"}, Type: &ListType{Kind: LIST_TYPE, Token: token.Token{}, Type: &NamedType{Kind: "Name", Token: token.Token{}, Name: &Name{Kind: "Name", Token: token.Token{}, Value: "String"}}}},
+			"name: [String]",
+		},
+		{
+			InputValueDefinition{Name: &Name{Kind: "Name", Token: token.Token{}, Value: "name"}, Type: &NonNullType{Kind: NONNULL_TYPE, Token: token.Token{}, Type: &ListType{Kind: LIST_TYPE, Token: token.Token{}, Type: &NonNullType{Kind: NONNULL_TYPE, Token: token.Token{}, Type: &NamedType{Kind: "Name", Token: token.Token{}, Name: &Name{Kind: "Name", Token: token.Token{}, Value: "String"}}}}}},
+			"name: [String!]!",
+		},
+	}
+
+	for i, obj := range test {
+
+		if obj.ivd.String() != obj.expectedOutput {
+			t.Errorf("%d wrong output,expected=%q, got=%q", i, obj.expectedOutput, obj.ivd.String())
+		}
+	}
+
+}
