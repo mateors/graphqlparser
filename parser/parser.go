62183b749db4f58f49962c6850e3b99f1de8a5f9
Mostain Billah <bill.rassel@gmail.com>
2022-10-24 16:15:27 +0600

    parser utility func added

--- parser/parser.go
+++ parser/parser.go
@@ -66,3 +66,26 @@ func (p *Parser) parseObjectDefinition() ast.Node {
 	od := &ast.ObjectDefinition{}
 	return od
 }
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		//p.peekError(t)
+		return false
+	}
+}
